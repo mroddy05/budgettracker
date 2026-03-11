9a7eca38f2ca5b3dad21c60eb1d66ca3a6c0cd19
mroddy05 <mroddy@unomaha.edu>
2026-03-10 19:21:20 -0500

    Bug Fixes and further styling

--- expenses/views.py
+++ expenses/views.py
@@ -64,7 +64,65 @@ def budget_overview_values():
     return (budget_overview, total_monthly_income, total_expenses)
 
 
+def month_logic():
+    today = date.today()
+    if today.month == 1:
+        last_month = 12
+        last_year = today.year - 1
+    else:
+        last_month = today.month - 1
+        last_year = today.year
+
+    lm_expenses = Expense.objects.filter(frequency = 'MONTHLY',
+        date__month=last_month,
+        date__year=last_year)
+    for exp in lm_expenses:
+        
+        if_exists = Expense.objects.filter(
+            expense_name=exp.expense_name,
+            date__month=today.month,
+            date__year=today.year,
+            date__day=exp.date.day).exists()
+        
+        if not if_exists:
+            Expense.objects.create(
+                expense_name=exp.expense_name,
+                expense_type=exp.expense_type,
+                amount=exp.amount,
+                date=today.replace(day=exp.date.day),
+                frequency=exp.frequency
+            )
+
+def year_logic():
+    today = date.today()
+    last_year = today.year - 1
+
+    ly_expenses = Expense.objects.filter(
+        frequency='YEARLY',
+        date__year=last_year,
+        date__month=today.month
+    )
+
+    for exp in ly_expenses:
+        if_exists = Expense.objects.filter(
+            expense_name=exp.expense_name,
+            date__year=today.year,
+            date__month=exp.date.month,
+            date__day=exp.date.day
+        ).exists()
+
+        if not if_exists:
+            Expense.objects.create(
+                expense_name=exp.expense_name,
+                expense_type=exp.expense_type,
+                amount=exp.amount,
+                date=exp.date.replace(year=today.year),
+                frequency=exp.frequency
+            )
+
 def index(request):
+    month_logic()
+    year_logic()
     budget_overview, total_monthly_income, total_expenses = budget_overview_values()  
     expense_list = Expense.objects.all().order_by('-date')
     paginator = Paginator(expense_list, 10)
@@ -76,6 +134,8 @@ def index(request):
         "index.html",
         {
             "expenses": page_obj,
+            "expenseTypes": Expense.expenseTypes,
+            "frequencyTypes": Expense.frequencyTypes,
             "budget_overview": budget_overview,
             "total_monthly_income": total_monthly_income,
             "total_expenses": total_expenses
@@ -199,6 +259,8 @@ def edit_expense(request, expense_id, page_number):
         "index.html",
         {
         "budget_overview": budget_overview,
+        "expenseTypes": Expense.expenseTypes,
+        "frequencyTypes": Expense.frequencyTypes,
         "total_monthly_income": total_monthly_income,
         "total_expenses": total_expenses,
         "expenses": page_obj,
